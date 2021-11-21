6fb54c38dfcd4b8e3dcfe98bf299d3e1a1a11ab2
Leozinco <leozinco123@hotmail.com>
2021-11-21 19:48:05 -0300

    adicionado drawer e content

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,20 @@
      v-app-bar
     </v-app-bar>
 
+    <v-main>
+        <v-row style="height: 100%; margin: 0">
+            <v-navigation-drawer permanent style="display: flex; align-items: center; justify-content: center"
+            >
+              <div style="height: 100%; display: flex; align-items: center;">v-navigation-drawer</div>
+            </v-navigation-drawer>
+            <v-content style="padding: 0">
+              <div style="height: 100%; display: flex; align-items: center; justify-content: center"
+              >v-content</div>
+            </v-content>
+        
+        </v-row>
+    </v-main>
+    
   </v-app>
 </template>
 
